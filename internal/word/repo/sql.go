0f543d736c493b6f963a8c1e3c07fcf51c3957ea
Maxorella <maxorella@mail.ru>
2025-04-12 17:08:17 +0300

    fix sql get rand_word

--- internal/word/repo/sql.go
+++ internal/word/repo/sql.go
@@ -6,10 +6,10 @@ const (
 	UploadLinkSql              = `UPDATE word_etalon SET audio_id = $1 WHERE word = $2 AND is_deleted = FALSE;`
 	GetWordCountSql            = `SELECT COUNT(*) from word_etalon;`
 	SelectRandomWordSql        = `SELECT word_id, word, transcription, tags, audio_id   FROM word_etalon ORDER BY RANDOM() LIMIT 1;`
-	SelectRandomWordWithTagSql = `SELECT word_id, word, transcription, tags, audio_id FROM word_etalon WHERE is_deleted = FALSE AND (',' || tags || ',') LIKE '%,' || $1 || ',%' ORDER BY RANDOM() LIMIT 1;`
+	SelectRandomWordWithTagSql = `SELECT word_id, word, transcription, tags, audio_id FROM word_etalon WHERE is_deleted = FALSE AND tags=$1 ORDER BY RANDOM() LIMIT 1;`
 	InsertTag                  = `INSERT INTO word_tag (tag) VALUES ($1) ON CONFLICT (tag) DO NOTHING;`
 	SelectTags                 = `SELECT word_tag FROM word_tag;`
-	SelectAllWordsWithTag      = `SELECT word_id, word, transcription, audio_id, tags FROM word_etalon WHERE ',' || tags || ',' LIKE '%,' || $1 || ',%' AND is_deleted = FALSE;`
+	SelectAllWordsWithTag      = `SELECT word_id, word, transcription, audio_id, tags FROM word_etalon WHERE tags=$1 AND is_deleted = FALSE;`
 
 	Insert1Stat        = `INSERT INTO word_user_try (word_id, result) VALUES ($1, $2);`
 	InsertPlusBigStat  = `INSERT INTO user_word_summary (word_id, total_plus, total_minus) VALUES ($1, 1, 0) ON CONFLICT (word_id) DO UPDATE SET total_plus = user_word_summary.total_plus + 1;`
